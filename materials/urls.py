ac2acbb809f48ef92e188e3514eacfb5bc97d53d
Valerie Gidzinski <valgidzi@gmail.com>
2019-01-04 12:00:30 -0800

    Change views to classes, update urls

--- materials/urls.py
+++ materials/urls.py
@@ -3,8 +3,8 @@ from rest_framework.urlpatterns import format_suffix_patterns
 from materials import views
 
 urlpatterns = [
-    path('materials/', views.material_list),
-    path('materials/<int:pk>/', views.material_detail),
+    path('materials/', views.MaterialList.as_view()),
+    path('materials/<int:pk>/', views.MaterialDetail.as_view()),
 ]
 
 urlpatterns = format_suffix_patterns(urlpatterns)
--- materials/views.py
+++ materials/views.py
@@ -1,104 +1,50 @@
 from rest_framework import status
-from rest_framework.decorators import api_view
+from rest_framework.views import APIView
 from rest_framework.response import Response
 from materials.models import Material
 from materials.serializers import MaterialSerializer
+from django.http import Http404
 
-
-@api_view(['GET', 'POST'])
-def material_list(request, format=None):
+class MaterialList(APIView):
     """
-    List all code materials, or create a new material.
+    List all snippets, or create a new snippet.
     """
-    if request.method == 'GET':
+    def get(self, request, format=None):
         materials = Material.objects.all()
         serializer = MaterialSerializer(materials, many=True)
-        response = Response(serializer.data)
-        response['Access-Control-Allow-Origin'] = '*'
-        return response
+        return Response(serializer.data)
 
-    elif request.method == 'POST':
+    def post(self, request, format=None):
         serializer = MaterialSerializer(data=request.data)
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
-@api_view(['GET', 'PUT', 'DELETE'])
-def material_detail(request, pk, format=None):
+class MaterialDetail(APIView):
     """
     Retrieve, update or delete a code material.
     """
-    try:
-        material = Material.objects.get(pk=pk)
-    except Material.DoesNotExist:
-        return Response(status=status.HTTP_404_NOT_FOUND)
+    def get_object(self, pk):
+        try:
+            return Material.objects.get(pk=pk)
+        except Material.DoesNotExist:
+            raise Http404
 
-    if request.method == 'GET':
+    def get(self, request, pk, format=None):
+        material = self.get_object(pk)
         serializer = MaterialSerializer(material)
         return Response(serializer.data)
 
-    elif request.method == 'PUT':
+    def put(self, request, pk, format=None):
+        material = self.get_object(pk)
         serializer = MaterialSerializer(material, data=request.data)
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
-    elif request.method == 'DELETE':
+    def delete(self, request, pk, format=None):
+        material = self.get_object(pk)
         material.delete()
         return Response(status=status.HTTP_204_NO_CONTENT)
-
-# from django.shortcuts import render
-#
-# from django.http import HttpResponse, JsonResponse
-# from django.views.decorators.csrf import csrf_exempt
-# from rest_framework.renderers import JSONRenderer
-# from rest_framework.parsers import JSONParser
-# from materials.models import Material
-# from materials.serializers import MaterialSerializer
-#
-# # Create your views here.
-# @csrf_exempt
-# def material_list(request):
-#     """
-#     List all materials, or create a new material.
-#     """
-#     if request.method == 'GET':
-#         materials = Material.objects.all()
-#         serializer = MaterialSerializer(materials, many=True)
-#         return JsonResponse(serializer.data, safe=False)
-#
-#     elif request.method == 'POST':
-#         data = JSONParser().parse(request)
-#         serializer = MaterialSerializer(data=data)
-#         if serializer.is_valid():
-#             serializer.save()
-#             return JsonResponse(serializer.data, status=201)
-#         return JsonResponse(serializer.errors, status=400)
-#
-# @csrf_exempt
-# def material_detail(request, pk):
-#     """
-#     Retrieve, update or delete a material.
-#     """
-#     try:
-#         material = Material.objects.get(pk=pk)
-#     except Material.DoesNotExist:
-#         return HttpResponse(status=404)
-#
-#     if request.method == 'GET':
-#         serializer = MaterialSerializer(material)
-#         return JsonResponse(serializer.data)
-#
-#     elif request.method == 'PUT':
-#         data = JSONParser().parse(request)
-#         serializer = MaterialSerializer(material, data=data)
-#         if serializer.is_valid():
-#             serializer.save()
-#             return JsonResponse(serializer.data)
-#         return JsonResponse(serializer.errors, status=400)
-#
-#     elif request.method == 'DELETE':
-#         material.delete()
-#         return HttpResponse(status=204)
