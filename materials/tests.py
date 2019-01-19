6b438f9b7a822acdbf33dc36684ea2758f335d9c
Valerie Gidzinski <valgidzi@gmail.com>
2019-01-18 16:54:57 -0800

    Update test for Handout model

--- materials/tests.py
+++ materials/tests.py
@@ -1,8 +1,8 @@
 from django.urls import reverse
 from rest_framework.test import APITestCase, APIClient
 from rest_framework.views import status
-from .models import Material
-from .serializers import MaterialSerializer
+from .models import Handout
+from .serializers import HandoutSerializer
 
 # tests for views
 
@@ -11,30 +11,29 @@ class BaseViewTest(APITestCase):
     client = APIClient()
 
     @staticmethod
-    def create_material(title="", level=""):
-        if title != "" and level != "":
-            Material.objects.create(title=title, level=level)
+    def create_handout(text="", words="", definitions="", user="", score="", title=""):
+        if text != "" and words != "" and definitions != "" and user != "" and score != "" and title != "":
+            Handout.objects.create(text=text, words=words, definitions=definitions, user=user, score=score, title=title)
 
     def setUp(self):
         # add test data
-        self.create_material("test title 1", "a1")
-        self.create_material("test title 2", "a2")
-        self.create_material("test title 3", "b1")
-        self.create_material("test title 4", "b2")
+        self.test1 = self.create_handout("test1", "test1", "test1", "test1", "test1", "test1")
+        self.test2 = self.create_handout("test 2", "test 2", "test 2", "test 2", "test 2", "test 2")
+        self.test3 = self.create_handout("test 3", "test 3", "test 3", "test 3", "test 3", "test 3")
 
 
-class GetAllMaterialTest(BaseViewTest):
+class GetAllHandoutTest(BaseViewTest):
 
-    def test_get_all_materials(self):
+    def test_get_all_handouts(self):
         """
-        This test ensures that all materials added in the setUp method
-        exist when we make a GET request to the materials/ endpoint
+        This test ensures that all handouts added in the setUp method
+        exist when we make a GET request to the handouts/ endpoint
         """
         # hit the API endpoint
         response = self.client.get(
-            '/materials/')
+            '/handouts/')
         # fetch the data from db
-        expected = Material.objects.all()
-        serialized = MaterialSerializer(expected, many=True)
+        expected = Handout.objects.all()
+        serialized = HandoutSerializer(expected, many=True)
         self.assertEqual(response.data, serialized.data)
         self.assertEqual(response.status_code, status.HTTP_200_OK)
